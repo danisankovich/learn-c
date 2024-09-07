5a9e16c993cba5ca002dcc2280778211b3981377
danisankovich <misankovich@gmail.com>
2024-09-07 13:18:04 -0400

    first

---
+++ arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[5];
+
+    numbers[0] = 8;
+    numbers[1] = 4;
+    numbers[2] = 1;
+    numbers[3] = 22;
+    numbers[4] = 5;
+
+    printf("This is number %d", numbers[3]);
+    return 0;
+}
